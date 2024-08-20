3f937697994e1d208120c542ab81aeaf0803b82a
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-08-20 08:58:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+     <h1 className='bold text-[48px]'>Hello</h1>
+   </div>
+  )
+}
+
+export default App
