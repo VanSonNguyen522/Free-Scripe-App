30844211e10f05450e044475b9a46c1546ad5cbf
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-08-27 14:34:46 +0700

    Information.tsx and animation for transcribing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react'
 import HomePage from './Components/HomePage'
 import Header from './Components/Header'
 import FileDisplay from './Components/FileDisplay'
+import Information from './Components/Information'
+import Transcribing from './Components/Transcribing'
 function App() {
   const [file, setFile] = useState(0)
   const [audioStream, setAudioStream] = useState(0)
   const isAudioAvaiable = file || audioStream
-
+  const [output, setOutput] = useState(null)
+  const [loading, setLoading] = useState(true)
   function handleAudioReset () {
     setFile(null)
     setAudioStream(null)
@@ -18,11 +21,18 @@ function App() {
    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
     <section className='min-h-screen flex flex-col'>
       <Header/>
-      {isAudioAvaiable ? (
-        <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream = {setAudioStream} />
-      ): (
-      <HomePage setFile = {setFile} setAudioStream = {setAudioStream}/>
-      )}
+      {
+        output ? (
+          <Information/>
+
+        ) : loading ? (
+          <Transcribing/>
+        ) : isAudioAvaiable ? (
+          <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream = {setAudioStream}/>
+        ) : (
+          <HomePage setFile = {setFile} setAudioStream = {setAudioStream}/>
+        )
+      }
     </section>
      <h1 className='text-green-400'>Hello</h1>
      <footer>
