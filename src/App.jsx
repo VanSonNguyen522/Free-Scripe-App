294d6fa0a8dfc685e47a93ecc94aebbd67fc6cae
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-08-29 10:51:34 +0700

    Upload some function in App.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,83 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import HomePage from './Components/HomePage'
 import Header from './Components/Header'
 import FileDisplay from './Components/FileDisplay'
 import Information from './Components/Information'
 import Transcribing from './Components/Transcribing'
+import { MessageTypes } from './utils/preset'
 function App() {
   const [file, setFile] = useState(0)
   const [audioStream, setAudioStream] = useState(0)
   const isAudioAvaiable = file || audioStream
-  const [output, setOutput] = useState(true)
-  const [loading, setLoading] = useState(true)
+  const [output, setOutput] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [dowloading, setDowloading] = useState(false)
+  const [finish, setFinish] = useState(false)
   function handleAudioReset () {
     setFile(null)
     setAudioStream(null)
   }
+  // useEffect(() => {
+  //   console.log(audioStream)
+  // }, [audioStream])
+
+  const worker = useRef(null)
+
   useEffect(() => {
-    console.log(audioStream)
-  }, [audioStream])
+    if(!worker.current) {
+      worker.current = new Worker(new URL('./utils/whisper.worker.js', import.meta.url), {
+        type: 'module'
+      })
+    }
+    const onMessageReceived = async (e) => {
+      switch (e.data.type) {
+        case 'DOWLOADING':
+          setDowloading(true)
+          console.log('DOWLOADING')
+          break;
+
+        case 'LOADING':
+          setLoading(true)
+          console.log('LOADING')
+          break;
+
+        case 'RESULT':
+          setOutput(e.data.results)
+          break;
+
+        case 'INFERENT_DONE':
+          setFinish(true)
+          console.log('DONE')
+          break;
+          
+      }
+    }
+
+    worker.current.addEventListener('message', onMessageReceived)
+    return () => worker.current.removeEventListener('message', onMessageReceived)
+
+  })
+
+  async function readAudioFrom(file) {
+    const sampling_rate = 16000
+    const audioCTX = new AudioContext({sampleRate:sampling_rate})
+    const response = await file.arrayBuffer()
+    const decoded = await audioCTX.decodeAudioData(response)
+    const audio = decoded.getChannelData(0)
+    return audio
+  }
+
+  async function handleFormSubmission() {
+    if(!file && !audioStream) {return}
+    let audio = await readAudioFrom(file ? file : audioStream)
+    const model_name = `openai/whisper-tiny.en`
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name
+    })
+  }
+
   return (
    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
     <section className='min-h-screen flex flex-col'>
