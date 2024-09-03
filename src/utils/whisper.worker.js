39f7e49f21bcbe43157096b6c6a9f3fa62a818c6
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-09-03 15:34:04 +0700

    finish

--- src/utils/whisper.worker.js
+++ src/utils/whisper.worker.js
@@ -1,108 +1,133 @@
-import {pipeline} from '@xenova/transformers'
-import { MessageTypes } from './preset'
+import { pipeline } from '@xenova/transformers'
+import { MessageTypes } from './presets'
 
-class MyTranscriptionPipeLine {
+class MyTranscriptionPipeline {
     static task = 'automatic-speech-recognition'
     static model = 'openai/whisper-tiny.en'
     static instance = null
 
-    static async getInstance(process_callBack = null)
-    {   if(this.instance === null)
-        {
-            this.instance = await pipeline(this.task, null, {progress_callback})
+    // static async getInstance(progress_callback = null) {
+    //     if (this.instance === null) {
+    //         this.instance = await pipeline(this.task, null, { progress_callback })
+    //     }
+
+    //     return this.instance
+    // }
+    static async getInstance(progress_callback = null) {
+        if (this.instance === null) {
+            try {
+                console.log("Initializing pipeline with model:", this.model);
+                this.instance = await pipeline(this.task, this.model, { progress_callback });
+                console.log("Pipeline initialized with model:", this.model);
+            } catch (error) {
+                console.error("Pipeline initialization failed:", error);
+            }
         }
-        return this.instance
+
+        if (!this.instance) {
+            console.error("Pipeline instance is still null after initialization.");
+        }
+
+        return this.instance;
+
+        async function getModelJSON(modelURL) {
+            try {
+                const response = await fetch(modelURL);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                return data;
+            } catch (error) {
+                console.error("Error fetching model JSON:", error);
+            }
+        }
+        
     }
 }
 
 self.addEventListener('message', async (event) => {
-    const {type, audio} = event.data
-    if(type === MessageTypes.INFERENCE_REQUEST)
+    const { type, audio } = event.data
+    if (type === MessageTypes.INFERENCE_REQUEST) {
         await transcribe(audio)
+    }
 })
 
 async function transcribe(audio) {
-    sendLoadingMessage('Loading')
-    
+    sendLoadingMessage('loading')
+
     let pipeline
 
     try {
-        pipeline = await MyTranscriptionPipeLine.getInstance(load_model_callBack)
-    }
-    catch(err)
-    {
+        pipeline = await MyTranscriptionPipeline.getInstance(load_model_callback)
+    } catch (err) {
         console.log(err.message)
     }
 
-    sendLoadingMessage('Success')
+    sendLoadingMessage('success')
 
     const stride_length_s = 5
+
     const generationTracker = new GenerationTracker(pipeline, stride_length_s)
     await pipeline(audio, {
         top_k: 0,
         do_sample: false,
-        chunk_length_s: 30,
+        chunk_length: 30,
         stride_length_s,
-        return_timestamps:true,
+        return_timestamps: true,
         callback_function: generationTracker.callbackFunction.bind(generationTracker),
-        chunk_callback: generationTracker.chunkCallBack.bind(generationTracker)
-
+        chunk_callback: generationTracker.chunkCallback.bind(generationTracker)
     })
     generationTracker.sendFinalResult()
 }
 
-async function load_model_callBack(data)
-{
-    const {status} = data
-    if(status === 'process')
-    {
-        const {file, process, loaded, total} = data
-        sendDowloadingMessage(file, process, loaded, total) 
+async function load_model_callback(data) {
+    const { status } = data
+    if (status === 'progress') {
+        const { file, progress, loaded, total } = data
+        sendDownloadingMessage(file, progress, loaded, total)
     }
 }
 
-function sendDowloadingMessage(status)
-{
+function sendLoadingMessage(status) {
     self.postMessage({
         type: MessageTypes.LOADING,
         status
     })
 }
 
-async function sendLoadingMessage(file, process, loaded, total)
-{
+async function sendDownloadingMessage(file, progress, loaded, total) {
     self.postMessage({
         type: MessageTypes.DOWNLOADING,
         file,
-        process,
+        progress,
         loaded,
         total
     })
 }
 
-class GenerationTracker{
-    constructor(pipeline, stride_length_s)
-    {
+class GenerationTracker {
+    constructor(pipeline, stride_length_s) {
         this.pipeline = pipeline
         this.stride_length_s = stride_length_s
         this.chunks = []
         this.time_precision = pipeline?.processor.feature_extractor.config.chunk_length / pipeline.model.config.max_source_positions
         this.processed_chunks = []
-        this.callbackFunctionCounter =  0
+        this.callbackFunctionCounter = 0
     }
 
     sendFinalResult() {
-        self.postMessage({type: MessageTypes.INFERENCE_DONE})
+        self.postMessage({ type: MessageTypes.INFERENCE_DONE })
     }
 
-    callbackFunction(beams){
+    callbackFunction(beams) {
         this.callbackFunctionCounter += 1
-        if(this.callbackFunctionCounter % 10 !== 0) {
+        if (this.callbackFunctionCounter % 10 !== 0) {
             return
         }
 
-        const bestBeams =  beams[0]
-        let text = this.pipeline.tokenizer.decode(bestBeams, output_token_ids,{
+        const bestBeam = beams[0]
+        let text = this.pipeline.tokenizer.decode(bestBeam.output_token_ids, {
             skip_special_tokens: true
         })
 
@@ -112,12 +137,13 @@ class GenerationTracker{
             end: undefined
         }
 
-        createPartiaResultMessage(result)
+        createPartialResultMessage(result)
     }
-    chunkCallBack(data) {
+
+    chunkCallback(data) {
         this.chunks.push(data)
-        const [text, {chunks}] = this.pipeline.tokenizer._decode_asr(
-            this.chunks, 
+        const [text, { chunks }] = this.pipeline.tokenizer._decode_asr(
+            this.chunks,
             {
                 time_precision: this.time_precision,
                 return_timestamps: true,
@@ -126,29 +152,47 @@ class GenerationTracker{
         )
 
         this.processed_chunks = chunks.map((chunk, index) => {
-            return this.processed_chunks(chunk, index)
+            return this.processChunk(chunk, index)
         })
+
 
         createResultMessage(
             this.processed_chunks, false, this.getLastChunkTimestamp()
         )
-
-       
     }
 
     getLastChunkTimestamp() {
-        if(this.processed_chunks.length === 0)
-        {
+        if (this.processed_chunks.length === 0) {
             return 0
         }
     }
-    processChunk(chunk, index) {
-        const{text, timestamp} = chunk
-        const[start, end ] = timestamp
-        
-        return {
-            
-        }
-    }
 
+    processChunk(chunk, index) {
+        const { text, timestamp } = chunk
+        const [start, end] = timestamp
+
+        return {
+            index,
+            text: `${text.trim()}`,
+            start: Math.round(start),
+            end: Math.round(end) || Math.round(start + 0.9 * this.stride_length_s)
+        }
+
+    }
+}
+
+function createResultMessage(results, isDone, completedUntilTimestamp) {
+    self.postMessage({
+        type: MessageTypes.RESULT,
+        results,
+        isDone,
+        completedUntilTimestamp
+    })
+}
+
+function createPartialResultMessage(result) {
+    self.postMessage({
+        type: MessageTypes.RESULT_PARTIAL,
+        result
+    })
 }
